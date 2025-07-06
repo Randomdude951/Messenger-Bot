2e8f2be8ef354bdd34e1f2a6a2a091a5be52ee2f
Tyler Franklin <tfrank951@gmail.com>
2025-07-06 12:35:30 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(bodyParser.json());
+
+app.get('/', (req, res) => {
+  res.send('Messenger bot is running');
+});
+
+app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
